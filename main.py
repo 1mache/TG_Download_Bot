eade5cba2a2803f9c91dfbe933fc9694a5d763d7
Imache <miktor4925@gmail.com>
2022-08-22 14:06:34 +0300

    Add start/help

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import telebot
+
+with open("key.txt", "r") as file:
+    BOT_KEY = file.read()
+
+if __name__ == "__main__":
+    bot = telebot.TeleBot(BOT_KEY)
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message):
+    bot.send_message(message.chat.id ,"Please send Youtube/Spotify link")
