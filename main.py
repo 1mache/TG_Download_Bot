bf18221dbf9261eefddf69abb3fdb68d569c5626
Imache <miktor4925@gmail.com>
2022-08-22 14:40:01 +0300

    Start link recognizing process

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import telebot
+import requests
 
 with open("key.txt", "r") as file:
     BOT_KEY = file.read()
 
-if __name__ == "__main__":
-    bot = telebot.TeleBot(BOT_KEY)
+bot = telebot.TeleBot(BOT_KEY)
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
     bot.send_message(message.chat.id ,"Please send Youtube/Spotify link")
+
+@bot.message_handler()
+def get_link(message):
+    mtext = message.text
+    if(mtext.startswith("https://www.youtube.com/watch?") or mtext.startswith("https://youtu.be/")):
+        print(requests.get(mtext).text)
+    else:
+        bot.send_message(message.chat.id ,"Please send a valid Youtube/Spotify link")
+        
+bot.infinity_polling()
